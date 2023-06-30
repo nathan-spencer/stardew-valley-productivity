b9df45f5ec34af51d46c79df3ea9966e5a1a72c1
nathan-spencer <n8spencer.g@gmail.com>
2023-06-29 18:12:42 -0600

    no papaparse

--- src/service/stardewService.ts
+++ src/service/stardewService.ts
@@ -1,33 +1,17 @@
 import Papa from "papaparse";
 import { ICrop } from "../interface/ICrop";
 import IObject from "../interface/IObject";
+import Crops from "./Crops";
+import Objects from "./Objects";
 
-async function getCrops() {
-  let crops: ICrop[] = [];
-  const response = await fetch("/data/csv/Crops.csv");
-  if (!response?.body) return crops;
-  const reader = response.body.getReader();
-  const result = await reader?.read(); // raw array
-  const decoder = new TextDecoder("utf-8");
-  const csv = decoder.decode(result.value); // the csv text
-  const results = Papa.parse<ICrop>(csv, { header: true }); // object with { data, errors, meta }
-  crops = results.data; // array of objects
-  return crops;
+async function getCrops(): Promise<ICrop[]> {
+  return Crops;
 }
 
-async function getObjects() {
-  let objects: IObject[] = [];
-  const response = await fetch("/data/csv/Objects.csv");
-  if (!response?.body) return objects;
-  const reader = response.body.getReader();
-  const result = await reader?.read(); // raw array
-  const decoder = new TextDecoder("utf-8");
-  const csv = decoder.decode(result.value); // the csv text
-  const results = Papa.parse<IObject>(csv, { header: true }); // object with { data, errors, meta }
-  objects = results.data; // array of objects
-  return objects;
+async function getObjects(): Promise<IObject[]> {
+  return Objects;
 }
-
+/*
 async function getSeedSources() {
   //Price in Objects.csv are what the seeds SELL for. Seeds general cost 2x what they sell for with the following exceptions.
   //431 Sunflower Seeds 200g @ Pierre's, 125g @ JojaMart. Also produces seeds which is weird.
@@ -54,18 +38,8 @@ async function getSeedSources() {
 
   //Egg Festival
   //745
-
-  let objects: IObject[] = [];
-  const response = await fetch("/data/csv/Objects.csv");
-  if (!response?.body) return objects;
-  const reader = response.body.getReader();
-  const result = await reader?.read(); // raw array
-  const decoder = new TextDecoder("utf-8");
-  const csv = decoder.decode(result.value); // the csv text
-  const results = Papa.parse<IObject>(csv, { header: true }); // object with { data, errors, meta }
-  objects = results.data; // array of objects
-  return objects;
 }
+*/
 
 const stardewService = { getCrops, getObjects };
 
